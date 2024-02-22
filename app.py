0b1810dad20533474b1f5ecc7cccba15ad423b4c
jaadeoye <82865605+jaadeoye@users.noreply.github.com>
2024-02-22 16:09:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ else:
     st.image(image, use_column_width=True)
     st.caption('_Image Uploaded by_ USER')
     img_array = preprocess_input(get_img_array(path, size=img_size))
-    prediction0 = model.predict(img_array)
+    prediction0 = model(img_array)
     prediction1 = np.array(prediction0)
     prediction2 = pd.DataFrame(prediction1)
     platt = pickle.load(open('lr', 'rb'))
